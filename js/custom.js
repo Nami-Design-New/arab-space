c4da1287128bbed1e405ab94c4abc9f4936d3fc3
mahmoudelkomy996 <94573066+namisoftgroup@users.noreply.github.com>
2023-02-27 14:13:13 +0200

    start

---
+++ js/custom.js
@@ -0,0 +1,130 @@
+$(document).ready(function () {
+  // nav menu
+  $(".navBtn").click(function () {
+    $(this).toggleClass("active");
+    $(".navMenu").toggleClass("active");
+  });
+  //navbar ainmation
+  $(window).scroll(function () {
+    var appScroll = $(document).scrollTop();
+    if (appScroll >= 1) {
+      $("header").addClass("headerAnimate");
+    } else {
+      $("header").removeClass("headerAnimate");
+    }
+  });
+  //appsSlider
+  var appsSlider = new Swiper(".appsSlider", {
+    spaceBetween: 10,
+    // loop: true,
+    // effect: "fade",
+    speed: 1000,
+    // autoplay: {
+    //   delay: 2000,
+    //   disableOnInteraction: false,
+    // },
+    scrollbar: {
+      el: ".swiper-scrollbar",
+      dragSize: 70,
+      draggable: true,
+      hide: false,
+    },
+    navigation: {
+      nextEl: ".appsSliderNext",
+      prevEl: ".appsSliderPrev",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 2,
+      },
+      467: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      992: {
+        slidesPerView: 3,
+      },
+      1200: {
+        slidesPerView: 4,
+      },
+    },
+  });
+  //feedbackSlider
+  var feedbackSlider = new Swiper(".feedbackSlider", {
+    spaceBetween: 16,
+    // loop: true,
+    // effect: "fade",
+    speed: 1000,
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".feedbackSliderPagination",
+      type: "fraction",
+    },
+    navigation: {
+      nextEl: ".feedbackSliderNext",
+      prevEl: ".feedbackSliderPrev",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1200: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+
+});
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+// ////////////////////////////////////////
+$(document).ready(function () {
+  //spinner
+  $(".spinner").fadeOut("slow");
+  // odometer
+  $(".odometer").appear(function (e) {
+    var odo = $(".odometer");
+    odo.each(function () {
+      var countNumber = $(this).attr("data-count");
+      $(this).html(countNumber);
+    });
+  });
+  // select2
+  $(".select2search").select2();
+  $(".select2").select2({
+    minimumResultsForSearch: -1,
+  });
+  //aos Delay
+  $("section").each(function () {
+    const sectionDivs = $(this).find("[data-aos]");
+    sectionDivs.each(function (index) {
+      $(this).attr("data-aos-delay", (index + 1) * 150);
+    });
+  });
+  // aos
+  AOS.init({
+    offset: 20,
+    delay: 50,
+    duration: 700,
+    // easing: "linear",
+    once: false,
+  });
+});
